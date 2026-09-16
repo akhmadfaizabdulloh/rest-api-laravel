4d9231f34a167d0cbbc7d16fe1e9124ca840d6d9
Akhmad Faiz Abdulloh <akhmadfaizabdulloh@gmail.com>
2026-09-16 22:05:33 +0700

    03 - API Resource & Eager Loading

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts2/{id}', [PostController::class, 'show2']);
+
